050848d5f199eb6f9b861aa2459d2cb798287afc
freecaykes <edbertcandra@gmail.com>
2017-05-20 12:16:11 -0700

    Removing prints

--- test/test_trivial.py
+++ test/test_trivial.py
@@ -6,6 +6,8 @@ from parallelsbtree import ParallelSBTree
 
 class test_simple(unittest.TestCase):
 
+    NUM_THREADS = 100000
+
     def merge_function(self, e1, e2):
         # print "merged: ", str( e1 + e2 )
         return e1 + e2
@@ -20,8 +22,8 @@ class test_simple(unittest.TestCase):
         return element
 
     def test_pbst_small_set(self):
-        NUM_THREADS = 10
-        to_sum = [int(1000*random.random()) for _ in xrange(NUM_THREADS)]
+        small_num_T = 10
+        to_sum = [int(1000*random.random()) for _ in xrange(small_num_T)]
         num_trees = ParallelSBTree(to_sum, 3, None)
 
         # blocking until done
@@ -32,8 +34,7 @@ class test_simple(unittest.TestCase):
         self.assertEqual(add_sum, sum_native)
 
     def test_pbst_1_leaf(self):
-        NUM_THREADS = 1000
-        to_sum = [int(1000*random.random()) for _ in xrange(NUM_THREADS)]
+        to_sum = [int(1000*random.random()) for _ in xrange(self.NUM_THREADS)]
         num_trees = ParallelSBTree(to_sum, 1000, None)
 
         # blocking until done
@@ -44,8 +45,7 @@ class test_simple(unittest.TestCase):
         self.assertEqual(add_sum, sum_native)
 
     def test_pbst_large_order(self):
-        NUM_THREADS = 1000
-        to_sum = [int(1000*random.random()) for _ in xrange(NUM_THREADS)]
+        to_sum = [int(1000*random.random()) for _ in xrange(self.NUM_THREADS)]
         num_trees = ParallelSBTree(to_sum, 10, None)
 
         # blocking until done
@@ -56,11 +56,9 @@ class test_simple(unittest.TestCase):
         self.assertEqual(add_sum, sum_native)
 
     def test_pbst_small_order(self):
-        NUM_THREADS = 1000
-        to_sum = [int(1000*random.random()) for _ in xrange(NUM_THREADS)]
+        to_sum = [int(1000*random.random()) for _ in xrange(self.NUM_THREADS)]
         num_trees = ParallelSBTree(to_sum, 3, None)
-        print to_sum
-        print str(num_trees._psbt)
+
         # blocking until done
         add_sum = num_trees.foreach(self.none_function, num_trees._psbt._root, self.merge_function)
 
